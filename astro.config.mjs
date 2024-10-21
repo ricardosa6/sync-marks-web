34d8226ac5dc90536bd678153950b084e01ea9ec
Ricardo Sánchez Arias <r.sanchezarias6@gmail.com>
2024-10-21 21:15:43 +0200

    animation for header scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -8,5 +8,9 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+  },
 });

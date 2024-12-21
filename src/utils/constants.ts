7b47e40c255cb3153fcfb2330fa707d40c789eb2
Ricardo Sánchez Arias <r.sanchezarias6@gmail.com>
2024-12-21 12:33:57 +0100

    add chrome extension url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const EXTENSION_GITHUB_REPO_URL =
   "https://github.com/ricardosa6/sync-marks";
+
+export const EXTENSION_URL_CHROME =
+  "https://chromewebstore.google.com/detail/sync-marks/ofdpahnnhcoajegbaanggjoepnpnpkmg";
